bbea02f4347f95e77358829482c073ee550cd514
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-03 06:24:42 +0700

    Add distro: Array-Compare-2.12.1; add authors's comments and blog posts

--- lib/Acme/CPANModules/OneAndTwoDecimalDigitsVersionTrap.pm
+++ lib/Acme/CPANModules/OneAndTwoDecimalDigitsVersionTrap.pm
@@ -35,9 +35,65 @@ and also <pm:version>.
 _
     entries => [
         # sorted by time, from most recent
-        {module=>"Validate::Simple", summary=>"From 0.01 to 0.01.1", date=>"2020-01-01", author=>"ANDREIP"},
-        {module=>"Bencher", summary=>"From 0.46 to 0.46.1", date=>"2016-03-31", author=>"PERLANCAR"},
-        {module=>"Acme::CPANLists", summary=>"From 0.02 to 0.9.0", date=>"2015-10-23", author=>"PERLANCAR"},
+
+        {
+            module=>"Validate::Simple",
+            summary=>"From 0.01 to 0.01.1",
+            date=>"2020-01-01",
+            author=>"ANDREIP",
+            description=><<'_',
+
+Author's comment in ChangeLog: "Change version properly."
+
+_
+        },
+
+        {
+            module=>"Bencher",
+            summary=>"From 0.46 to 0.46.1",
+            date=>"2016-03-31",
+            author=>"PERLANCAR",
+            description=><<'_',
+
+Author's comment in ChangeLog: "This version number is broken because 0.46 >
+0.46.1 because 0.46 normalizes to 0.460.000 while 0.46.1 is 0.046.100. This has
+happened a few times to me in other distributions too, so perhaps it's time to
+consider switching to a 3-digit minor version."
+
+Author's blog post:
+<https://perlancar.wordpress.com/2018/09/10/should-i-choose-x-yy-or-x-yyy-versioning-scheme-for-my-perl-module/>
+(2016-09-10).
+
+_
+        },
+
+        {
+            module=>"Array::Compare",
+            summary=>"From 2.12 to 2.12.1",
+            date=>"2016-12-08",
+            author=>"DAVECROSS",
+            description=><<'_',
+
+Author's comment in ChangeLog: "Fixing the idiocy in the previous version."
+
+Author's blog post: <https://perlhacks.com/2016/12/version-numbers/>
+(2016-12-13).
+
+_
+        },
+
+        {
+            module=>"Acme::CPANLists",
+            summary=>"From 0.02 to 0.9.0",
+            date=>"2015-10-23",
+            author=>"PERLANCAR",
+            description=><<'_',
+
+Author's comment in ChangeLog: "Update version number so it's higher than 0.02
+(d'oh)."
+
+_
+        },
     ],
 };
 
